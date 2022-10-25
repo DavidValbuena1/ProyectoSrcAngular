32f4358abd9202c2f5e0d7b3b0c1abd4c7b1ae8a
DavidValbuena1 <104107409+DavidValbuena1@users.noreply.github.com>
2022-10-25 13:58:45 -0500

    Add files via upload

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,50 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { EmailValidator } from '@angular/forms';
-import { Router } from '@angular/router';
-import Swal from 'sweetalert2';
-import { UsuarioService } from '../servicios/usuario.service';
+import { Component, OnInit } from "@angular/core";
+import { EmailValidator } from "@angular/forms";
+import { Router } from "@angular/router";
+import Swal from "sweetalert2";
+import { UsuarioService } from "../servicios/usuario.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  constructor(private router: Router, private usuarioservice: UsuarioService) {}
 
-  constructor(private router:Router, private usuarioservice:UsuarioService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  email: any;
+  password: any;
+
+  formularioInicio:boolean=true;
+  formularioRecuperar:boolean=false;
+  iniciarSesion() {
+    let data = {
+      correo: this.email,
+      contraseña: this.password,
+    };
+    this.usuarioservice.verificarUsuario(data).subscribe((x: any) => {
+      localStorage.setItem("usuarioConectado", "");
+      console.log(x);
+      
+      if (x == null) {
+        Swal.fire("¡Upss!", "El correo y la contraseña no coinciden", "error");
+      } else if (x.rol.id == 1) {
+        this.router.navigate(["administrador"]);
+      } else if (x.rol.id == 2) {
+        this.router.navigate(["vendedor"]);
+      }
+      localStorage.setItem("usuarioConectado", x.rol.id);
+      localStorage.setItem("usuarioNombre", x.nombres + " " + x.apellidos);
+    });
   }
 
-  email:any;
-  password:any;
-  iniciarSesion(){
-    let data={
-      correo:this.email,
-      contraseña:this.password
-    }
-    this.usuarioservice.verificarUsuario(data).subscribe((x:any)=>{
-      localStorage.setItem("usuarioConectado","");
-      console.log(x);
+  IrARecuperarContrasena() {
+    this.formularioInicio=false;
+    this.formularioRecuperar=true;
+  }
 
-      localStorage.setItem("usuarioConectado",x.rol.id);
-      localStorage.setItem("usuarioNombre",x.nombres + " " + x.apellidos);
-
-
-      try{
-        if(x.rol.id == 1){
-          this.router.navigate(['administrador']);
-        }
-        else if(x.rol.id == 2){
-          this.router.navigate(['vendedor']);
-        }
-        }catch(err){
-          Swal.fire(
-            '¡Upss!',
-            'El correo y la contraseña no coinciden',
-            'error'
-          )
-        }
-    })
+  enviarCorreoRecuperar(){
+    
   }
 }
